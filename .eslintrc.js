871fab1a06169584ff8a6d78076e22d955c12a6f
Anonymous <idontwanttoshare@mymail.com>
2022-07-10 23:20:00 +0700

    fix: lint rules

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  parser: "@babel/eslint-parser",
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [],
+  parserOptions: {
+    sourceType: "module",
+  },
+  rules: {},
+};
